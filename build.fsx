7a08673ce2176d8349bec1af6bd241fcaa461a84
Alfonso Garcia-Caro <alfonso.garcia-caro@outlook.com>
2021-10-07 13:50:24 +0900

    Use Fable.Expect

--- build.fsx
+++ build.fsx
@@ -1,7 +1,9 @@
-#load "node_modules/fable-publish-utils/PublishUtils.fs"
+#r "nuget: Fable.PublishUtils, 2.4.0"
 
 open System
 open PublishUtils
+
+run "npm install && npm test"
 
 // ATTENTION: Packages must appear in dependency order
 let packages =
@@ -28,24 +30,23 @@ let packages =
 let ignoreCaseEquals (str1: string) (str2: string) =
     String.Equals(str1, str2, StringComparison.OrdinalIgnoreCase)
 
+let args =
+    fsi.CommandLineArgs
+    |> Array.skip 1
+    |> List.ofArray
+
 match args with
 | IgnoreCase "publish"::rest ->
     let target = List.tryHead rest
     let srcDir = fullPath "src"
-    let projFiles =
-        // [ for file in dirFiles srcDir do
-        //     let projDir = srcDir </> file
-        //     if isDirectory projDir then
-        //         for file in dirFiles projDir do
-        //             if file.EndsWith(".fsproj") then
-        //                 yield projDir, file ]
-        [ for pkg in packages do
-            yield (srcDir </> pkg), ("Browser." + pkg + ".fsproj") ]
+    let projFiles = packages |> List.map (fun pkg ->
+        (srcDir </> pkg), ("Browser." + pkg + ".fsproj"))
 
     for projDir, file in projFiles do
-        match target with
-        | Some target ->
-            if ignoreCaseEquals file.[..(file.Length - 8)] target then
-                pushFableNuget (projDir </> file) [] doNothing
-        | None -> pushFableNuget (projDir </> file) [] doNothing
+        let publish =
+            match target with
+            | Some target -> ignoreCaseEquals file.[..(file.Length - 8)] target
+            | None -> true
+        if publish then
+            pushFableNuget (projDir </> file) [] doNothing
 | _ -> ()
--- test/EventTest.fs
+++ test/EventTest.fs
@@ -10,22 +10,40 @@ open Browser.Types
 
 describe "Event" <| fun () ->
     it "can create events" <| fun () -> promise {
-        use! container = render_html $"""<div></div>"""
-        let mutable x = 0
-        container.El.addEventListener("my-event", fun _ -> x <- x + 1)
+        use container = Container.New()
+
+        let mutable sideEffect = 0
+        container.El.addEventListener("my-event", fun _ ->
+            sideEffect <- sideEffect + 1)
+
+        let ev = Event.Create("my-event", jsOptions(fun o ->
+            o.bubbles <- true))
+        container.El.appendChild().dispatchEvent(ev) |> ignore
+        sideEffect |> Expect.equal 1
+
+        // Non-bubbling events won't be detected by parent
         let ev = Event.Create("my-event")
+        container.El.appendChild().dispatchEvent(ev) |> ignore
+        sideEffect |> Expect.equal 1
+
+        // But can be detected on the same element
         container.El.dispatchEvent(ev) |> ignore
-        x |> Expect.equal 1
+        sideEffect |> Expect.equal 2
     }
 
     it "can create custom events" <| fun () -> promise {
-        use! container = render_html $"""<div></div>"""
-        let mutable x = 0
+        use container = Container.New()
+
+        let mutable sideEffect = 0
         container.El.addEventListener("my-custom-event", fun ev ->
             let ev = ev :?> CustomEvent<int>
-            ev.detail |> Expect.some "event detail" |> fun v -> x <- x + v)
+            ev.detail |> Expect.some "event detail" |> fun v ->
+                sideEffect <- sideEffect + v)
+
         let ev = CustomEvent.Create("my-custom-event", jsOptions<CustomEventInit<int>>(fun o ->
+            o.bubbles <- true
             o.detail <- Some 5))
-        container.El.dispatchEvent(ev) |> ignore
-        x |> Expect.equal 5
+        container.El.appendChild().dispatchEvent(ev) |> ignore
+
+        sideEffect |> Expect.equal 5
     }
