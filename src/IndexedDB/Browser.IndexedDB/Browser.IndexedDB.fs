fe6b703b67e5cc1a959a7542a9c54a3d9e80500c
Simen Kirkvik <simen.kirkvik@tromso.serit.no>
2022-09-26 16:52:28 +0200

    initial commit

---
+++ src/IndexedDB/Browser.IndexedDB/Browser.IndexedDB.fs
@@ -0,0 +1,24 @@
+﻿namespace Browser.Types
+
+open System
+open Fable.Core
+
+
+// type IDBCursorWithValue = ()
+// type IDBDatabase = ()
+type IDBFactory =
+    abstract ``open``: string with get
+// type IDBIndex = ()
+// type IDBKeyRange = ()
+// type IDBObjectStore = ()
+// type IDBOpenDBRequest = ()
+// type IDBRequest = ()
+// type IDBTransaction = ()
+// type IDBVersionChangeEvent = ()
+
+type IndexedDBType =
+    abstract ``open``: string -> unit
+
+[<AutoOpen>]
+module IndexedDB =
+    let [<Global>] indexedDB: IndexedDBType = jsNative
