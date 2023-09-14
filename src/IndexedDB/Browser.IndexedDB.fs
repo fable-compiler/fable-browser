a852bb3a4d2176d883ed1c89cfd7b99387403890
Robert Byrne <rob@kapitar.com>
2023-09-14 12:46:01 +0100

    Align IndexedDB bindings with the current spec

--- src/IndexedDB/Browser.IndexedDB.fs
+++ src/IndexedDB/Browser.IndexedDB.fs
@@ -14,6 +14,25 @@ type IDBRequestReadyState =
     | Pending
     | Done
 
+[<StringEnum>]
+type IDBTransactionMode =
+    | Readonly
+    | Readwrite
+    | Versionchange
+
+[<StringEnum>]
+type IDBTransactionDuarability =
+    | Strict
+    | Relaxed
+    | Default
+
+[<StringEnum>]
+type IDBCursorDirection =
+    | Next
+    | Nextunique
+    | Prev
+    | Prevunique
+
 type [<AllowNullLiteral>] DatabasesType =
     abstract name: string
     abstract version: string
@@ -32,8 +51,8 @@ type [<AllowNullLiteral; Global>] IDBIndex =
     abstract getKey: ?key: obj -> IDBRequest
     abstract getAll: ?query: obj * ?count: int -> IDBRequest
     abstract getAllKeys: ?query: obj * ?count: int -> IDBRequest
-    abstract openCursor: ?range: obj * ?direction: string -> IDBRequest
-    abstract openKeyCursor: unit -> IDBRequest
+    abstract openCursor: ?range: obj * ?direction: IDBCursorDirection -> IDBRequest
+    abstract openKeyCursor: ?range: obj * ?direction: IDBCursorDirection -> IDBRequest
 
 type [<AllowNullLiteral; Global>] IDBVersionChangeEvent =
     inherit Event
@@ -44,12 +63,13 @@ type [<AllowNullLiteral; Global>] IDBVersionChangeEvent =
 type [<AllowNullLiteral; Global>] IDBKeyRange =
     abstract lower: obj with get
     abstract upper: obj with get
-    abstract lowerOpen: obj with get
-    abstract upperOpen: obj with get
+    abstract lowerOpen: bool with get
+    abstract upperOpen: bool with get
+    abstract includes: obj -> bool
 
 type [<AllowNullLiteral; Global>] IDBCursor =
     abstract source: IDBObjectStore with get
-    abstract direction: string with get
+    abstract direction: IDBCursorDirection with get
     abstract key: obj option with get
     abstract primaryKey: obj option with get
     abstract request: IDBRequest with get
@@ -66,10 +86,12 @@ type [<AllowNullLiteral; Global>] IDBCursorWithValue =
     abstract value: obj option with get
 
 type [<AllowNullLiteral; Global>] IDBTransaction =
+    inherit EventTarget
+
     abstract db: IDBDatabase with get
-    abstract durability: obj with get
+    abstract durability: IDBTransactionDuarability with get
     abstract error: DOMException with get
-    abstract mode: string with get
+    abstract mode: IDBTransactionMode with get
     abstract objectStoreNames: DOMStringList with get
 
     abstract abort: unit -> unit
@@ -78,6 +100,7 @@ type [<AllowNullLiteral; Global>] IDBTransaction =
 
     abstract oncomplete: (Event -> unit) with get, set
     abstract onerror: (Event -> unit) with get, set
+    abstract onabort: (Event -> unit) with get, set
 
 type [<AllowNullLiteral; Global>] IDBRequest =
     inherit EventTarget
@@ -91,6 +114,10 @@ type [<AllowNullLiteral; Global>] IDBRequest =
     abstract onerror: (Event -> unit) with get, set
     abstract onsuccess: (Event -> unit) with get, set
 
+type [<AllowNullLiteral; Global>] IDBCreateIndexOptions =
+    abstract unique: bool with get, set
+    abstract multiEntry: bool with get, set
+
 type [<AllowNullLiteral; Global>] IDBObjectStore =
     abstract indexNames: DOMStringList with get
     abstract keyPath: obj with get
@@ -100,18 +127,25 @@ type [<AllowNullLiteral; Global>] IDBObjectStore =
 
     abstract add: value: obj * ?key: obj -> IDBRequest
     abstract clear: unit -> IDBRequest
-    abstract count: unit -> IDBRequest
-    abstract createIndex: indexName: string * keyPath: string * ?objectParameters: obj -> IDBRequest
-    abstract delete: unit -> IDBRequest
+    abstract count: ?query: IDBKeyRange -> IDBRequest
+    abstract createIndex: indexName: string * keyPath: string * ?options: IDBCreateIndexOptions -> IDBRequest
+    abstract delete: key: obj -> IDBRequest
     abstract deleteIndex: unit -> IDBRequest
-    abstract get: obj -> IDBRequest
-    abstract getKey: unit -> IDBRequest
-    abstract getAll: unit -> IDBRequest
-    abstract getAllKeys: unit -> IDBRequest
+    abstract get: key: obj -> IDBRequest
+    abstract getKey: key: obj -> IDBRequest
+    abstract getAll: ?query: IDBKeyRange * ?count: int -> IDBRequest
+    abstract getAllKeys: ?query: IDBKeyRange * ?count: int -> IDBRequest
     abstract index: string -> IDBIndex
-    abstract openCursor: ?query: IDBKeyRange -> IDBRequest
-    abstract openKeyCursor: unit -> IDBRequest
-    abstract put: unit -> IDBRequest
+    abstract openCursor: ?range: IDBKeyRange * ?direction: IDBCursorDirection -> IDBRequest
+    abstract openKeyCursor: ?range: IDBKeyRange * ?direction: IDBCursorDirection -> IDBRequest
+    abstract put: item: obj * ?key: obj -> IDBRequest
+
+type [<AllowNullLiteral; Global>] IDBCreateStoreOptions =
+    abstract keyPath: obj with get, set
+    abstract autoIncrement: bool with get, set
+
+type [<AllowNullLiteral; Global>] IDBTransactionOptions =
+    abstract durability: IDBTransactionDuarability with get, set
 
 type [<AllowNullLiteral; Global>] IDBDatabase =
     inherit EventTarget
@@ -121,21 +155,39 @@ type [<AllowNullLiteral; Global>] IDBDatabase =
     abstract objectStoreNames: DOMStringList with get
 
     abstract close: unit -> unit
-    abstract createObjectStore: name: string * ?options: obj -> IDBObjectStore
+    abstract createObjectStore: name: string * ?options: IDBCreateStoreOptions -> IDBObjectStore
     abstract deleteObjectStore: name: string -> unit
-    abstract transaction: storeNames: #seq<string> * ?mode: string * ?options: obj -> IDBTransaction
-    abstract transaction: storeNames: string * ?mode: string * ?options: obj -> IDBTransaction
+    abstract transaction: storeNames: #seq<string> * ?mode: IDBTransactionMode * ?options: IDBTransactionOptions -> IDBTransaction
+    abstract transaction: storeNames: string * ?mode: IDBTransactionMode * ?options: IDBTransactionOptions -> IDBTransaction
 
+    abstract onclose: (Event -> unit) with get, set
+    abstract onversionchange: (Event -> unit) with get, set
+    abstract onabort: (Event -> unit) with get, set
     abstract onerror: (Event -> unit) with get, set
 
 type [<AllowNullLiteral; Global>] IDBOpenDBRequest =
     inherit IDBRequest
 
     abstract blocked: (Event -> unit) with get, set
-    abstract onupgradeneeded: (Event -> unit) with get, set
+    abstract onupgradeneeded: (IDBVersionChangeEvent -> unit) with get, set
 
 type [<AllowNullLiteral; Global>] IDBFactory =
-    abstract ``open``: name: string * ?version: int64 -> IDBOpenDBRequest
+    abstract ``open``: name: string * ?version: int -> IDBOpenDBRequest
     abstract cmp: first: 'T * second: 'T -> int
     abstract deleteDatabase: name: string -> IDBOpenDBRequest
     abstract databases: unit -> JS.Promise<DatabasesType array>
+
+[<AutoOpen>]
+module Static =
+    type IDBKeyRange with
+        [<Emit("IDBKeyRange.bound($0, $1, $2, $3)")>]
+        static member bound(lower: obj, upper: obj, ?lowerOpen: bool, ?upperOpen: bool) = jsNative<IDBKeyRange>
+
+        [<Emit("IDBKeyRange.only($0)")>]
+        static member only(only: obj) = jsNative<IDBKeyRange>
+
+        [<Emit("IDBKeyRange.lowerBound($0, $1)")>]
+        static member lowerBound(lower: obj, ?``open``: bool) = jsNative<IDBKeyRange>
+
+        [<Emit("IDBKeyRange.upperBound($0, $1)")>]
+        static member upperBound(upper: obj, ?``open``: bool) = jsNative<IDBKeyRange>
